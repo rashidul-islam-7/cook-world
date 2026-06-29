7d2a5deecd200a96deb8af11b9f59a674cab4e8c
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-06-30 05:33:15 +0600

    update home page

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({ children }) {
     <div className="lg:flex">
       <Sidebar />
 
-      <main className="lg:flex-1 px-5 md:px-8 mt-18 py-10 md:py-20 dark:bg-gray-800">
+      <main className="lg:flex-1 px-5 md:px-8 mt-18 py-10  dark:bg-gray-800">
         {children}
       </main>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,19 @@
-export const metadata = {
-  title: "Home | CookWorld",
-};
+
 import React from 'react';
 import HomeHero from "@/components/HomePage/HomeHero";
 import FeaturedRecipes from '@/components/HomePage/FeaturedRecipes';
 import PopularRecipes from '@/components/HomePage/PopularRecipes';
 import PremiumGiftOffer from '@/components/HomePage/PremiumGiftOffer';
 import JoinCommunity from '@/components/HomePage/JoinWithUs';
-import LoadingPage from '@/components/Loading';
-import BrowseRecipesPage from './recipes/page';
 
-const HomePage = () => {
+export const dynamic = "force-dynamic";
+
+export const metadata = {
+  title: "Home | CookWorld",
+  description: "Welcome to CookWorld - The Ultimate Recipe Platform",
+};
+
+const HomePage = async() => {
   return (
     <div>
      <section className='pt-10'>
